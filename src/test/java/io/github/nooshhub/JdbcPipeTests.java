031c1cee0de1825305a2ec53977874750f661bdf
neals <shandeai520@gmail.com>
2022-06-09 06:44:02 -0700

    format as spring code style

--- src/test/java/io/github/nooshhub/JdbcPipeTests.java
+++ src/test/java/io/github/nooshhub/JdbcPipeTests.java
@@ -1,23 +1,23 @@
 /*
- *  Copyright 2021-2022 the original author or authors.
+ * Copyright 2021-2022 the original author or authors.
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *         https://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *   limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.github.nooshhub;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -38,16 +38,12 @@ public class JdbcPipeTests {
 
 	@Test
 	public void init() {
-		indexConfigRegistry.getIndexConfigs().forEach(indexConfig -> {
-			jdbcPipe.init(indexConfig);
-		});
+		this.indexConfigRegistry.getIndexConfigs().forEach((indexConfig) -> this.jdbcPipe.init(indexConfig));
 	}
 
 	@Test
 	public void sync() {
-		indexConfigRegistry.getIndexConfigs().forEach(indexConfig -> {
-			jdbcPipe.sync(indexConfig);
-		});
+		this.indexConfigRegistry.getIndexConfigs().forEach((indexConfig) -> this.jdbcPipe.sync(indexConfig));
 	}
 
 }
