04bd834a5387f9df24d0742da3f21fe483493f33
neals <shandeai520@gmail.com>
2022-06-25 23:00:43 -0700

    add unversioned files

---
+++ espipe-mq-consumer/src/main/java/io/github/nooshhub/espipemqconsumer/EspipeMqConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.github.nooshhub.espipemqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspipeMqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspipeMqConsumerApplication.class, args);
+	}
+
+}
---
+++ espipe-mq-consumer/src/test/java/io/github/nooshhub/espipemqconsumer/EspipeMqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nooshhub.espipemqconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspipeMqConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
